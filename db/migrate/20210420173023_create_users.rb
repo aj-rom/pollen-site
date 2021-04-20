c1a015027eb358a4223144c274eae560985ae11e
A.J. Romaniello <ajiellodev@gmail.com>
2021-04-20 17:33:54 -0600

    Update to PostgreSQL values for better performance.

--- db/migrate/20210420173023_create_users.rb
+++ db/migrate/20210420173023_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.string :email
+      t.text :email
       t.integer :uid
-      t.string :provider
+      t.text :provider
       t.timestamps
     end
   end
--- db/migrate/20210420174551_create_retailers.rb
+++ db/migrate/20210420174551_create_retailers.rb
@@ -2,7 +2,7 @@ class CreateRetailers < ActiveRecord::Migration[6.1]
   def change
     create_table :retailers do |t|
       t.belongs_to :user
-      t.string :name, unique: true
+      t.text :name, unique: true
       t.text :description
 
       t.timestamps
