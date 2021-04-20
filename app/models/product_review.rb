41b6858dec1931fc3e112076d0490e198c39fb22
A.J. Romaniello <ajiellodev@gmail.com>
2021-04-20 12:56:14 -0600

    Fix min and max for description datatype to integer instead of string

--- app/models/product_review.rb
+++ app/models/product_review.rb
@@ -5,7 +5,7 @@ class ProductReview < ApplicationRecord
   belongs_to :user
   has_one :retailer, through: :product
 
-  validates_length_of :description, minimum: '20', maximum: '3000',
+  validates_length_of :description, minimum: 20, maximum: 3000,
                                     message: 'Review must be between 20 - 3000 characters'
   validates_numericality_of :rating, minimum: 1, maximum: 5
 end
