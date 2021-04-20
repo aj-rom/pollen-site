16a3fc8e6028502ec9799900ac431faa40c0e852
A.J. Romaniello <ajiellodev@gmail.com>
2021-04-20 12:33:37 -0600

    Create product reviews table

--- db/migrate/20210420173122_create_product_reviews.rb
+++ db/migrate/20210420173122_create_product_reviews.rb
@@ -1,7 +1,8 @@
 class CreateProductReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :product_reviews do |t|
-
+      t.belongs_to :user
+      t.belongs_to :product
       t.timestamps
     end
   end
