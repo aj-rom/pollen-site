07ac5a5f33f69c45eac2bfc6e4eeaebef0e65e85
A.J. Romaniello <ajiellodev@gmail.com>
2021-04-20 12:33:16 -0600

    Add validations to retailers and stub out its migration

--- app/models/retailer.rb
+++ app/models/retailer.rb
@@ -1,2 +1,14 @@
+# frozen_string_literal: true
+
 class Retailer < ApplicationRecord
+  # Name validations
+  validates_uniqueness_of :name, message: 'A retailer by this name already exists.'
+  validates_length_of :name, maximum: 255
+
+  # Description Validations
+  validates_presence_of :description, message: 'Please provide a description of your products/services.'
+  validates_length_of :description,
+                      minimum: 100,
+                      maximum: 3000,
+                      message: 'Description must be between 100 - 3000 characters'
 end
--- db/migrate/20210420174551_create_retailers.rb
+++ db/migrate/20210420174551_create_retailers.rb
@@ -1,6 +1,9 @@
 class CreateRetailers < ActiveRecord::Migration[6.1]
   def change
     create_table :retailers do |t|
+      t.belongs_to :user
+      t.string :name, unique: true
+      t.text :description
 
       t.timestamps
     end
