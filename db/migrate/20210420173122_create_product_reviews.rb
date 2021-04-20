a5ca86a0e6a7e6825512a81408cd788e7e9c29e2
A.J. Romaniello <ajiellodev@gmail.com>
2021-04-20 12:36:21 -0600

    Add rating and description to product reviews

--- db/migrate/20210420173122_create_product_reviews.rb
+++ db/migrate/20210420173122_create_product_reviews.rb
@@ -3,6 +3,8 @@ class CreateProductReviews < ActiveRecord::Migration[6.1]
     create_table :product_reviews do |t|
       t.belongs_to :user
       t.belongs_to :product
+      t.text :description
+      t.integer :rating
       t.timestamps
     end
   end
