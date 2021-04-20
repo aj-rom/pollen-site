ed0a3eb3e19d635132f96ed1ee74026604692859
A.J. Romaniello <ajiellodev@gmail.com>
2021-04-20 12:36:00 -0600

    Add validations to product reviews

--- app/models/product_review.rb
+++ app/models/product_review.rb
@@ -1,2 +1,5 @@
 class ProductReview < ApplicationRecord
+  validates_length_of :description, minimum: '20', maximum: '3000',
+                                    message: 'Review must be between 20 - 3000 characters'
+  validates_numericality_of :rating, minimum: 1, maximum: 5
 end
