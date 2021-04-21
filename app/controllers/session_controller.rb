9b4d39b517863d6c1328599d2da3a164654f4eef
A.J. Romaniello <ajiellodev@gmail.com>
2021-04-21 03:08:15 -0600

    Add session managment

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,10 @@
+class SessionController < ApplicationController
+
+  def login
+  end
+
+  def logout
+    session.clear
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'session/login'
+
+  post 'session/logout'
   resources :retailers, only: %i[index show new create edit update delete]
   root 'static#home'
 
