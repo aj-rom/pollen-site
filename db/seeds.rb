425e2d8d8874f703bc5d388ffad0d546b16f9e97
A.J. Romaniello <ajiellodev@gmail.com>
2021-04-21 15:56:47 -0600

    Add seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+admin = User.create!(email: 'ajiellodev@gmail.com', provider: 'github', uid: 7357807)
+
+r1 = admin.retailers.build(name: 'Sustainable Co',
+                      description: 'A sustainable alternative to your average carbon polluting products.')
+r1.save
+r1.products.build(name: 'Test', price: 34.65, description: 'Just a description of this product.')
