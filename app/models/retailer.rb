5bf21da4dbfa8ed87d026d23c563b9a692909465
A.J. Romaniello <ajiellodev@gmail.com>
2021-04-21 16:26:44 -0600

    Add name validations

--- app/models/retailer.rb
+++ app/models/retailer.rb
@@ -6,15 +6,16 @@ class Retailer < ApplicationRecord
   has_many :product_reviews, through: :products
 
   # Name validations
-  validates_uniqueness_of :name, message: 'A retailer by this name already exists.'
-  validates_length_of :name, maximum: 255
+  validates_format_of :name, without: /(^\s)/, message: 'must not start with space.'
+  validates_uniqueness_of :name, message: 'must be unique.'
+  validates_length_of :name, maximum: 255, message: 'must be less than 256 characters.'
 
   # Description Validations
-  validates_presence_of :description, message: 'Please provide a description of your products/services.'
+  validates_presence_of :description, message: 'must be present.'
   validates_length_of :description,
                       minimum: 20,
                       maximum: 3000,
-                      message: 'Description must be between 20 - 3000 characters'
+                      message: 'must be between 20 - 3000 characters'
 
   def average_review
     count = product_reviews.count.to_f
