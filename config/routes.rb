8c21fd59d8a0120b84496eba9d341a0bc674b348
A.J. Romaniello <ajiellodev@gmail.com>
2021-04-21 03:18:23 -0600

    Add link to login or sign out from homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'session/login'
-
-  post 'session/logout'
-  resources :retailers, only: %i[index show new create edit update delete]
   root 'static#home'
-
+  get '/login', as: :login, controller: 'session', action: 'login'
+  post '/logout', as: :logout, controller: 'session', action: 'logout'
+  resources :retailers, only: %i[index show new create edit update delete]
 end
