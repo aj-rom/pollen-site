6c47350a47a504e2caadc377f963bc1c8193bd62
A.J. Romaniello <ajiellodev@gmail.com>
2021-04-22 12:20:05 -0600

    Add validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,9 @@ class Product < ApplicationRecord
   belongs_to :retailer
   has_many :product_reviews
   has_many :users, through: :product_reviews
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :description, presence: true, length: { minimum: 20, maximum: 1000 }
+
+  validates_numericality_of :price, format: { in: /(\d{1,9}.\d{2})/}
 end
