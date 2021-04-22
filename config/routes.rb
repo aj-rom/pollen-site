9d00c4b82a697a82f9483dd56ffbc7a49f7929ec
A.J. Romaniello <ajiellodev@gmail.com>
2021-04-22 01:01:25 -0600

    Add products as nester resourece

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   root 'static#home'
-  resources :retailers, only: %i[index show new create edit update delete]
+  resources :retailers, only: %i[index show new create edit update delete] do
+    resources :products, only: %i[index show new create edit update delete]
+  end
 
   # Devise
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   devise_scope :user do
-    get 'login', :to => 'users/session#login', :as => :new_session
-    get 'logout', :to => 'devise/sessions#destroy', :as => :destroy_session
+    get 'login', to: 'users/session#login', :as => :new_session
+    get 'logout', to: 'devise/sessions#destroy', :as => :destroy_session
   end
   post '/logout', as: :logout, to: 'users/session#logout'
 end
