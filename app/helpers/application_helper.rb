62c946a463277068662c09739c9adb1ed0c1367d
A.J. Romaniello <ajiellodev@gmail.com>
2021-04-23 16:13:07 -0600

    DRY up error explanation views for retialers and products

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,9 @@ module ApplicationHelper
   def nav_link(text, path)
     render inline: "<li>#{link_to text, path}</li>"
   end
+
+  def error_fields(resource)
+    @resource = resource
+    render partial: 'layouts/errors'
+  end
 end
