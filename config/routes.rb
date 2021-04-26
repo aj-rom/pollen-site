5477fe4cb690a812600711cda31b901233b6f71c
A.J. Romaniello <ajiellodev@gmail.com>
2021-04-26 13:07:36 -0600

    Add user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  resources :users, only: %i[index show new create edit update destroy]
   root 'static#home'
   get '/retailers/most_products', to: 'retailers#most_products', as: :retailer_most_products
-  resources :retailers, only: %i[index show new create edit update] do
-    resources :products, only: %i[index show new create edit update]
+  resources :retailers, only: %i[index show new create edit update destroy] do
+    resources :products, only: %i[index show new create edit update destroy]
     delete '/products/:id', to: 'products#destroy', as: 'delete_product'
   end
   delete '/retailers/:id', to: 'retailers#destroy', as: 'delete_retailer'
@@ -13,5 +14,9 @@ Rails.application.routes.draw do
     get 'login', to: 'users/session#login', :as => :new_session
     get 'logout', to: 'devise/sessions#destroy', :as => :destroy_session
   end
+
   post '/logout', as: :logout, to: 'users/session#logout'
+
+  # Users
+  resources :users, only: %i[index show new create edit update destroy]
 end
