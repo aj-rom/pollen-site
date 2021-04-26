49c7d2fa3f069ec930fa90cb0c39ccdb9323d14f
A.J. Romaniello <ajiellodev@gmail.com>
2021-04-26 13:39:54 -0600

    Add validations for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,10 @@ class User < ApplicationRecord
   has_many :product_reviews
   has_many :products, through: :product_reviews
   devise :omniauthable, omniauth_providers: %i[github]
-  validates :email, email: true
+
+  has_secure_password
+
+  validates :email, email: true, uniqueness: true
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
