9ecdf88452940153cfc8fa7d0c712f2f76e712ae
A.J. Romaniello <ajiellodev@gmail.com>
2021-04-26 14:53:49 -0600

    Add bcrypt stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  include BCrypt
   has_many :retailers
   has_many :product_reviews
   has_many :products, through: :product_reviews
@@ -13,6 +14,19 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
+      pass = Devise.friendly_token(20)
+      user.password = pass
+      user.password_confirmation = pass
     end
   end
+
+  def password
+    @password ||= Password.new(password_digest)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_digest = @password
+  end
+
 end
