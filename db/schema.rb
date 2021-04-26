076d723b656f1f19cf649b92c0f9b84f7b846de2
A.J. Romaniello <ajiellodev@gmail.com>
2021-04-26 13:39:24 -0600

    Update user schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_21_213031) do
+ActiveRecord::Schema.define(version: 2021_04_26_191746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,8 @@ ActiveRecord::Schema.define(version: 2021_04_21_213031) do
     t.string "provider"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
