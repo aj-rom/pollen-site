fbca826fc86a65f556680faf8132277b39ec7674
A.J. Romaniello <ajiellodev@gmail.com>
2021-04-26 14:54:09 -0600

    remove bad migration

--- db/migrate/20210426191746_add_username_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddUsernameToUsers < ActiveRecord::Migration[6.1]
-  def change
-    add_column :users, :password_digest, :string
-    add_column :users, :admin, :boolean, default: false
-  end
-end
